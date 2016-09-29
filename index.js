df29c749d2882862efd4331c5b09180f9aad8637
nagendra007 <nagendra.singh@ninedots.com>
2016-09-29 16:18:31 +0530

    test
    test

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-
-var braintree = require("braintree");
-var gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    merchantId: "bt9cmxxgmv9smhng",
-    publicKey: "5rsr9yzrgp7ztmbz",
-    privateKey: "31e804049ca424d8c0ffcf31cf2a86e5"
-});
-
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -22,22 +12,14 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  //databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-   databaseURI: databaseUri || 'mongodb://heroku_116xm8rv:6nqih6bihno17osraofmmp16ju@ds041586.mlab.com:41586/heroku_116xm8rv',
-   
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'test', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  push: {
-      android: {
-        senderId: '396448347452',
-        apiKey: 'AIzaSyBFOOgUxGnlB0CIwTxDwycw0wHD3jM2RS0'
-      }
-    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -62,17 +44,6 @@ app.get('/', function(req, res) {
 app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
-
-//app.get("/client_token", function (req, res) {
-//    gateway.clientToken.generate({}, function (err, response) {
-//        res.send(response.clientToken);
-//    });
-//});
-//app.post("/checkout", function (req, res) {
-//    var nonceFromTheClient = req.body.payment_method_nonce;
-    // Use payment method nonce here
-//});
-
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
